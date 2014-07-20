c84a8195e7eddcdd1e76b1b8213776952ed3cdd7
Katherina Nguyen <kathnn89@alumni.stanford.edu>
2014-07-19 21:15:46 -0700

    Initial Commit

---
+++ photo_pet_chef1Tests/photo_pet_chef1Tests.swift
@@ -0,0 +1,35 @@
+//
+//  photo_pet_chef1Tests.swift
+//  photo_pet_chef1Tests
+//
+//  Created by Katherina Nguyen on 7/19/14.
+//  Copyright (c) 2014 KNguyen. All rights reserved.
+//
+
+import XCTest
+
+class photo_pet_chef1Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
